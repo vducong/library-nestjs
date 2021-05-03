bc0d451520f83c99c4429adc833a1dcec01859ad
VCD <vdc1114@gmail.com>
2021-05-03 22:42:11 +0200

    feat: interceptor

---
+++ src/utils/interceptor/excludeNull.interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+ 
+@Injectable()
+export class ExcludeNullInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next
+      .handle()
+      .pipe(map(value => this.recursivelyStripNullValues(value)));
+  }
+
+  recursivelyStripNullValues(value: unknown): unknown {console.error(1);console.error(value)
+    if (Array.isArray(value)) {console.error(2);console.error(value)
+      return value.map(this.recursivelyStripNullValues);
+    }
+    if (value !== null && typeof value === 'object') {console.error(3);console.error(value)
+      return Object.fromEntries(
+        Object.entries(value).map(([key, value]) => [key, this.recursivelyStripNullValues(value)])
+      );
+    }
+    if (value !== null) {
+      console.error(5);console.error(value)
+      return value;
+    }
+  }
+}
