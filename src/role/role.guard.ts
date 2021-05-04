257cef4de195066d48853385beffa957ce6f999d
VCD <vdc1114@gmail.com>
2021-05-04 08:06:08 +0200

    feat: role decorator

---
+++ src/role/role.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthService } from 'src/auth/auth.service';
+import { ROLES_KEY } from './role.decorator';
+import { Role } from './role.enum';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly reflector: Reflector,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const token = request.headers.authorization.substring('Bearer '.length);
+    const user = await this.authService.verifyTokenJwt(token);
+
+    const roles = [];
+    if (user.isAdmin) roles.push(Role.USER, Role.ADMIN);
+    else roles.push(Role.USER);
+
+    return requiredRoles.some((role) => roles.includes(role));
+  }
+}
